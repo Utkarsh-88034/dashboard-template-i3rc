550af34a5fb966c0095e9cde49595546320405bc
svnitad <svnitad@gmail.com>
2022-01-13 13:22:28 +0530

    codes

---
+++ notus-react-main/src/Coding Notations/voter.status.js
@@ -0,0 +1,17 @@
+export const voterStatus = {
+    "1" : "Active Voter",
+    "2" : "Not in Position to Vote",
+    "3" : "Voter Shifted to Other Location",
+    "4" : "Voter Shifted to Other Booth",
+    "5" : "No more Voters",
+}
+export const education = {
+    "1" : "Active Voter",
+    "2" : "Not in Position to Vote",
+    "3" : "Voter Shifted to Other Location",
+    "4" : "Voter Shifted to Other Booth",
+    "5" : "No more Voters",
+    "6" : "No more Voters",
+    "7" : "No more Voters",
+    "8" : "No more Voters",
+}
