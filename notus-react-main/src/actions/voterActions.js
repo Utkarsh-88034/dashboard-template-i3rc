cf51b30cefbd7e7c636cbd5891855497ad3c74e3
svnitad <svnitad@gmail.com>
2022-01-28 00:33:39 +0530

    hfdhbdfh

--- notus-react-main/src/actions/voterActions.js
+++ notus-react-main/src/actions/voterActions.js
@@ -1,30 +1,51 @@
-import axios from 'axios';
+import axios from "axios";
 import {
-    VOTER_LIST_REQUEST,
-    VOTER_LIST_SUCCESS,
-    VOTER_LIST_FAIL,
-} from '../constants/userConstants';
+  VOTER_LIST_REQUEST,
+  VOTER_LIST_SUCCESS,
+  VOTER_LIST_FAIL,
+  VOTER_POST_REQUEST,
+  VOTER_POST_SUCCESS,
+  VOTER_POST_FAIL,
+} from "../constants/userConstants";
 
+export const listVoters = () => async (dispatch) => {
+  try {
+    dispatch({ type: VOTER_LIST_REQUEST });
 
-export const listVoters = () => async (
-    dispatch
-  ) => {
-    try {
-      dispatch({ type: VOTER_LIST_REQUEST })
-  
-      const { data } = await axios.get("/api/voter/voterdata")
-  
-      dispatch({
-        type: VOTER_LIST_SUCCESS,
-        payload: data,
-      })
-    } catch (error) {
-      dispatch({
-        type: VOTER_LIST_FAIL,
-        payload:
-          error.response && error.response.data.message
-            ? error.response.data.message
-            : error.message,
-      })
-    }
+    const { data } = await axios.get("/api/voter/voterdata");
+
+    dispatch({
+      type: VOTER_LIST_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: VOTER_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
   }
+};
+
+export const postVoterData = (voter) => async (dispatch) => {
+  try {
+    dispatch({ type: VOTER_POST_REQUEST });
+
+    const { data } = await axios.post("/api/voter/voterdata", voter);
+
+    dispatch({
+      type: VOTER_POST_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: VOTER_POST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
--- notus-react-main/src/reducers/voterReducers.js
+++ notus-react-main/src/reducers/voterReducers.js
@@ -1,21 +1,37 @@
 import {
-    VOTER_LIST_REQUEST,
-    VOTER_LIST_SUCCESS,
-    VOTER_LIST_FAIL,
-} from '../constants/userConstants';
+  VOTER_LIST_REQUEST,
+  VOTER_LIST_SUCCESS,
+  VOTER_LIST_FAIL,
+  VOTER_POST_REQUEST,
+  VOTER_POST_SUCCESS,
+  VOTER_POST_FAIL,
+} from "../constants/userConstants";
 
 export const voterListReducer = (state = { voters: [] }, action) => {
   switch (action.type) {
     case VOTER_LIST_REQUEST:
-      return { loading: true, voters: [] }
+      return { loading: true, voters: [] };
     case VOTER_LIST_SUCCESS:
       return {
         loading: false,
         voters: action.payload.data,
-      }
+      };
     case VOTER_LIST_FAIL:
-      return { loading: false, error: action.payload }
+      return { loading: false, error: action.payload };
     default:
-      return state
+      return state;
   }
-}
+};
+
+export const voterPostReducer = (state = {}, action) => {
+  switch (action.type) {
+    case VOTER_POST_REQUEST:
+      return { loading: true };
+    case VOTER_POST_SUCCESS:
+      return { loading: false, success: true, voters: action.payload };
+    case VOTER_POST_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
